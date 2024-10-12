f0e1a82dd52e1b70195d6e1f6818b0a6ed85a4a1
Telma <Telma@DESKTOP-6S05TII>
2024-10-12 15:58:04 +0200

    Creacion de Perro, atributos de instancia y de clase. Closes #2

--- GAME-PDOOG/src/Perro/Perro.java
+++ GAME-PDOOG/src/Perro/Perro.java
@@ -9,5 +9,19 @@ package Perro;
  * @author Telma
  */
 public class Perro {
+    //*****************************************************************
+    // Attributes 
+    //*****************************************************************
+    public static final String ICONO = "/img/perro.png";
+    public static final String NOMBRE = "Perro sin nombre"; 
+    public static final int EDAD = 1;
+    public static final int ATENCION = 0;
+    public static final int NIVEL = 10;
     
+    private String icono;
+    private String nombrePerro; 
+    private int edadPerro;
+    private int atencionPerro;
+    private int nivel;
+
 }
